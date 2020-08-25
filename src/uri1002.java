42fdf9483fcff46622b87c3c2a858623e92d66bb
Guilherme Zanini de Sá <zan.sued@gmail.com>
2020-08-24 21:37:28 -0300

    Update uri1002.java
    Ao invés de utilizar nomes sem significado, acrescentar nomes mais significativos. Também contextualizar o usuário com a entrada. Ao invés de utilizar as multiplicações diretamente para o cálculo da área, utilizar a classe Math.

--- src/uri1002.java
+++ src/uri1002.java
@@ -6,15 +6,16 @@ public class uri1002 {
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
+		Scanner entrada = new Scanner(System.in);
 		
-		double R, A, pi = 3.14159;
-	    
-	    R = sc.nextDouble();
-
-	    A = pi * R * R;
-
-	    System.out.printf("A=%.4f%n", A);
+		Double pi = 3.14159;
+		
+		System.out.println("Digite o tamanho do raio do círculo: ");
+		Double raio = entrada.nextDouble();
+		
+		Double areaDeUmCirculo = Math.pow(raio, 2) * pi;
+    
+	    	System.out.printf("O círculo com o raio %.4f possui uma área A=%.6f %n", raio, areaDeUmCirculo);
 
 		sc.close();
 	}
